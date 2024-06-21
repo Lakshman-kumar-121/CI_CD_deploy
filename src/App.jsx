a5a189c8e0bd7e0dd86ae4132d650cb9ac3b683b
Rakesh kumar <lakshmankumarr121@gmail.com>
2024-06-21 18:14:18 +0530

    added double

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ function App() {
       <h1>hello form</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 2)}>
-          inasd {count}
+          changed {count}
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
